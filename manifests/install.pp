1ee05c9e26757dbff84cdfa58f82b4e57730293c
root <root@tim-tindell-gmail-com3.mylabserver.com>
2017-05-06 20:53:47 +0000

    Made install and run pps

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,5 +1,8 @@
-class ssh::install{
-  package { 'openssh-server':
+class ssh::install(
+  String $package_name = $::ssh::package_name,
+){
+  package { 'ssh-package':
     ensure => present,
+    name   => $package_name,
  }
 }
--- manifests/service.pp
+++ manifests/service.pp
@@ -1,6 +1,9 @@
-class ssh::service {
-  service { 'sshd': 
+class ssh::service(
+ String $service_name = $::ssh::service_name,
+) {
+  service { 'ssh-service': 
     ensure     => running,
+    name       => $service_name,
     enable     => true,
     hasstatus  => true,
     hasrestart => true,
