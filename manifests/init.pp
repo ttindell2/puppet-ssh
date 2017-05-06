755b6e58ea05ddd7a677786824496f053dd05dab
root <root@tim-tindell-gmail-com3.mylabserver.com>
2017-05-06 22:13:53 +0000

    Added the config pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -46,8 +46,13 @@ class ssh (
   String $package_name = $::ssh::params::package_name,
   String $service_name = $::ssh::params::service_name,
 ) inherits ::ssh::params {
-  class { '::ssh::install': }->
+  class { '::ssh::install': }
+  class { '::ssh::config': }
   class { '::ssh::service':}
-
+  
+  Class['::ssh::install'] 
+  -> Class['::ssh::config']
+  ~> Class['::ssh::service']
+  -> Class['ssh']
 
 }
